d182b8f9299948398ea86813fda9a1acd899d012
Mantas M <mantasef@gmail.com>
2022-07-01 15:08:14 +0300

    routes + router + pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import AddPage from './pages/AddPage';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import NotFoundPage from './pages/NotFoundPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Heloo again world</h1>
+      <Switch>
+        <Route path={'/register'}>
+          <RegisterPage />
+        </Route>
+        <Route path={'/login'}>
+          <LoginPage />
+        </Route>
+        <Route path={'/add'}>
+          <AddPage />
+        </Route>
+        <Route exact path={'/'}>
+          <HomePage />
+        </Route>
+        <Route path={'*'}>
+          <NotFoundPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
