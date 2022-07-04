e00977b083c1fd237aee3086009fb509da3155a2
Mantas M <mantasef@gmail.com>
2022-07-04 12:24:48 +0300

    header + logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,41 @@
 import css from './Header.module.css';
 import { Link, NavLink } from 'react-router-dom';
+import { useAuthCtx } from '../../store/AuthContext';
 
 function Header(props) {
+  const { isUserLoggedIn, logout, userEmail } = useAuthCtx();
   return (
     <header className={css.header}>
       <NavLink to='/'>
         <img className={css.img} src='../img/skills.png' alt='logoNav' />
       </NavLink>
       <nav>
-        <NavLink className={css['nav-link']} to={'/'}>
-          Home
-        </NavLink>
-        <NavLink className={css['nav-link']} to={'/add'}>
-          Add
-        </NavLink>
-        <NavLink className={css['nav-link']} to={'/login'}>
-          Login
-        </NavLink>
-        <NavLink className={css['nav-link']} to={'/register'}>
-          Register
-        </NavLink>
+        {isUserLoggedIn && (
+          <>
+            <NavLink className={css['nav-link']} to={'/'}>
+              Home
+            </NavLink>
+            <NavLink className={css['nav-link']} to={'/add'}>
+              Add
+            </NavLink>
+            <NavLink onClick={logout} className={css['nav-link']} to={'/login'}>
+              Logout
+            </NavLink>
+            <a className={css['nav-link']} href='/'>
+              {userEmail}
+            </a>
+          </>
+        )}
+        {!isUserLoggedIn && (
+          <>
+            <NavLink className={css['nav-link']} to={'/login'}>
+              Login
+            </NavLink>
+            <NavLink className={css['nav-link']} to={'/register'}>
+              Register
+            </NavLink>
+          </>
+        )}
       </nav>
     </header>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import { useAuthCtx } from '../store/AuthContext';
 
 function HomePage() {
-  return <div>HomePage</div>;
+  const { isUserLoggedIn } = useAuthCtx();
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      {!isUserLoggedIn && <p>Jus esate neprisijunges, prasome prisiregistruoti</p>}
+      {isUserLoggedIn && <p>Sveiki atvyke!</p>}
+    </div>
+  );
 }
 
 export default HomePage;
